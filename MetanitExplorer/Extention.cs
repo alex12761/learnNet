5a39d1bb31f3fcd251aecf92ec7a5454e6636828
a_arhipov <a_arhipov@ugrasu.ru>
2022-08-25 15:55:05 +0500

    посмотрел на методы расширения

---
+++ MetanitExplorer/Extention.cs
@@ -0,0 +1,19 @@
+﻿namespace MetanitExplorer;
+
+// методы расширения предопределенных классов
+public static class TokenExtensions
+{
+    public static IApplicationBuilder UseToken(this IApplicationBuilder builder)
+    {
+        return builder.Use(async (context, next) =>
+        {
+            await context.Response.WriteAsync("middleware");
+            await next.Invoke();
+        });
+    }
+    
+    public new static string ToLower(this string builder)
+    {
+        return "new string";
+    }
+}
--- MetanitExplorer/Program.cs
+++ MetanitExplorer/Program.cs
@@ -1,6 +1,17 @@
+using MetanitExplorer;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var qwe = "QwE";
+var temp = qwe.ToLower();
+
+app.Map("/time", (appBuilder ) => { });
+app.UseToken();
+app.Use(async (HttpContext context, Func<Task> next) =>
+{
+    await context.Response.WriteAsync("Hello Work!");
+    // next.Invoke();
+});
 
 app.Run();
