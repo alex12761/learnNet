9b76a802549dcaaea67637c13dcdce28d03309bf
Alex12731 <arhipov_sasha127@mail.ru>
2023-06-13 18:57:17 +0500

    change string to lowwer

--- MetanitExplorer/Extention.cs
+++ MetanitExplorer/Extention.cs
@@ -14,6 +14,6 @@ public static class TokenExtensions
     
     public new static string ToLower(this string builder)
     {
-        return "new string123453411111111111hello world";
+        return "new string lets start again";
     }
 }
