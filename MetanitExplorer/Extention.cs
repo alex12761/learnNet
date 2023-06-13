637f11dd303076facd8294237c99dec2cec00cb7
alex12761 <106764606+alex12761@users.noreply.github.com>
2023-06-13 19:11:25 +0500

    Update Extention.cs

--- MetanitExplorer/Extention.cs
+++ MetanitExplorer/Extention.cs
@@ -7,7 +7,7 @@ public static class TokenExtensions
     {
         return builder.Use(async (context, next) =>
         {
-            await context.Response.WriteAsync("middleware5554321");
+            await context.Response.WriteAsync("middleware555432155555");
             await next.Invoke();
         });
     }
