d0a7d4bb4734b1cee140fa4b2c34666b90e68683
Alex12731 <arhipov_sasha127@mail.ru>
2023-06-27 20:10:43 +0500

    test

--- MetanitExplorer/Extention.cs
+++ MetanitExplorer/Extention.cs
@@ -7,7 +7,7 @@ public static class TokenExtensions
     {
         return builder.Use(async (context, next) =>
         {
-            await context.Response.WriteAsync("middleware");
+            await context.Response.WriteAsync("middleware123123123");
             await next.Invoke();
         });
     }
