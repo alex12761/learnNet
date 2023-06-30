72558930bc852b3e954f81203bf29b4577f3ae00
Alex12731 <arhipov_sasha127@mail.ru>
2023-06-30 19:34:36 +0500

    12312у1312412

--- MetanitExplorer/Extention.cs
+++ MetanitExplorer/Extention.cs
@@ -14,6 +14,6 @@ public static class TokenExtensions
     
     public new static string ToLower(this string builder)
     {
-        Console.WriteLine("test");
+        Console.WriteLine("test45453453453");
     }
 }
